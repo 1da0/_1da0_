69fa58ce885199e166babb9db567aa06989be160
1da0 <31944504+1da0@users.noreply.github.com>
2017-10-10 16:45:30 +0900

    Add files via upload

---
+++ day6_hw.py
@@ -0,0 +1,202 @@
+import turtle as t
+t.shape("turtle")
+
+t.speed(5)
+
+n = 6
+t.color("brown")
+t.begin_fill()
+for x in range(n):
+    t.fd(50)
+    t.lt(360/n)
+t.end_fill()
+
+t.lt(120)
+
+t.color("yellow")
+t.begin_fill()
+for x in range(n):
+    t.fd(50)
+    t.lt(360/n)
+t.end_fill()
+
+t.lt(120)
+
+t.color("yellow")
+t.begin_fill()
+for x in range(n):
+    t.fd(50)
+    t.lt(360/n)
+t.end_fill()
+
+t.lt(120)
+t.fd(50)
+t.rt(60)
+
+t.color("yellow")
+t.begin_fill()
+for x in range(n):
+    t.fd(50)
+    t.lt(360/n)
+
+t.end_fill()
+
+t.fd(50)
+t.rt(60)
+
+t.color("yellow")
+t.begin_fill()
+for x in range(n):
+    t.fd(50)
+    t.lt(360/n)
+t.end_fill()
+
+t.fd(50)
+t.rt(60)
+
+t.color("brown")
+t.begin_fill()
+for x in range(n):
+    t.fd(50)
+    t.lt(360/n)
+t.end_fill()
+
+t.fd(50)
+t.rt(60)
+
+t.color("yellow")
+t.begin_fill()
+for x in range(n):
+    t.fd(50)
+    t.lt(360/n)
+t.end_fill()
+
+t.fd(50)
+t.lt(60)
+t.fd(50)
+t.rt(60)
+
+t.color("brown")
+t.begin_fill()
+for x in range(n):
+    t.fd(50)
+    t.lt(360/n)
+t.end_fill()
+
+t.fd(50)
+t.rt(60)
+
+t.color("yellow")
+t.begin_fill()
+for x in range(n):
+    t.fd(50)
+    t.lt(360/n)
+t.end_fill()
+
+t.fd(50)
+t.rt(60)
+
+t.color("yellow")
+t.begin_fill()
+for x in range(n):
+    t.fd(50)
+    t.lt(360/n)
+t.end_fill()
+
+t.fd(50)
+t.lt(60)
+t.fd(50)
+t.rt(60)
+
+t.color("brown")
+t.begin_fill()
+for x in range(n):
+    t.fd(50)
+    t.lt(360/n)
+t.end_fill()
+
+t.fd(50)
+t.rt(60)
+
+t.color("yellow")
+t.begin_fill()
+for x in range(n):
+    t.fd(50)
+    t.lt(360/n)
+t.end_fill()
+
+t.fd(50)
+t.lt(60)
+t.fd(50)
+t.rt(60)
+
+t.color("brown")
+t.begin_fill()
+for x in range(n):
+    t.fd(50)
+    t.lt(360/n)
+t.end_fill()
+
+t.fd(50)
+
+t.up()
+t.fd(200)
+t.down()
+t.fd(10)
+t.rt(105)
+t.fd(20)
+t.rt(150)
+t.fd(20)
+t.rt(105)
+t.fd(5)
+
+t.color("yellow")
+t.begin_fill()
+t.circle(50)
+t.end_fill()
+
+t.up()
+t.lt(45)
+t.fd(90)
+t.down()
+t.color("brown")
+t.circle(25)
+
+t.rt(250)
+t.circle(25)
+
+t.up()
+t.lt(30)
+t.fd(120)
+t.down()
+t.circle(25)
+
+t.rt(190)
+t.circle(25)
+
+t.up()
+t.lt(45)
+t.fd(80)
+t.down()
+t.circle(20)
+
+t.up()
+t.lt(60)
+t.fd(35)
+t.rt(20)
+t.down()
+t.fd(50)
+t.rt(90)
+t.circle(5)
+
+t.up()
+t.rt(110)
+t.fd(60)
+t.down()
+t.rt(100)
+t.fd(50)
+t.rt(90)
+t.circle(5)
+
+t.up()
+t.fd(50)
